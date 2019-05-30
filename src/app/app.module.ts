a7e3ad59690c5d2655bf8137383818e2f0b35519
liweibo <925857462@qq.com>
2019-05-30 15:52:13 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Camera } from '@ionic-native/camera/ngx';
+import { HttpModule } from '@angular/http';
+    import { HttpClientModule, HttpClient } from '@angular/common/http';
+    import { HTTP } from '@ionic-native/http/ngx';
+import { from } from 'rxjs';
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
+    HttpModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Camera,
+    HTTP,
+    
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
